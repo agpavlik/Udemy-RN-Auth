13cff3175d2bbf288782784f2a45c463199e2715
agpavlik <agpavlik@gmail.com>
2024-05-25 23:57:44 -0400

    Clean old code

--- util/auth.js
+++ util/auth.js
@@ -16,18 +16,6 @@ async function authenticate(mode, email, password) {
 }
 
 // Send request to the Firebase to create a new user
-// export async function createUser(email, password) {
-//   const response = await axios.post(
-//     "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
-//     {
-//       email: email,
-//       password: password,
-//       returnSecureToken: true,
-//     }
-//   );
-// }
-
-// Send request to the Firebase to create a new user
 export async function createUser(email, password) {
   await authenticate("signUp", email, password);
 }
