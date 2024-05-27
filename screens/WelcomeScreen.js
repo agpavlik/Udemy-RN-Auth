051b7c6f65ac1e6bffdf8ba5a5767effa586bc07
agpavlik <agpavlik@gmail.com>
2024-05-27 00:12:59 -0400

    Add rules for protected database

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+
+import { AuthContext } from "../store/auth-context";
+import { useContext, useEffect, useState } from "react";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMesssage] = useState("");
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://rn-auth-37d40-default-rtdb.firebaseio.com/message.json?auth=" +
+          token
+      )
+      .then((response) => {
+        setFetchedMesssage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
